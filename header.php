b3704faa4cbc745142a1c1d782df4fa13699801e
robertallenbaker <robertallen@usa.com>
2016-11-19 20:31:15 -0500

    Changed Initial Scale to Materialize recommendation

--- header.php
+++ header.php
@@ -2,7 +2,7 @@
 <html <?php language_attributes(); ?>>
 <head>
 	<meta charset="<?php bloginfo( 'charset' ); ?>" />
-	<meta name="viewport" content="width=device-width" />
+	<meta name="viewport" content="width=device-width, initial-scale=1"/>
 	<title>
 		<?php bloginfo('name'); ?> |
 		<?php is_front_page() ? bloginfo('description') : wp_title(''); ?>
