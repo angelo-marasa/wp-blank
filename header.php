a75fb2797ed207283ea85edfbee62de3233c421f
robertallenbaker <robertallen@usa.com>
2017-02-06 19:29:52 -0500

    Update header.php

--- header.php
+++ header.php
@@ -2,7 +2,7 @@
 <html <?php language_attributes(); ?>>
 <head>
 	<meta charset="<?php bloginfo( 'charset' ); ?>" />
-	<meta name="viewport" content="width=device-width, initial-scale=1"/>
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 	<title>
 		<?php bloginfo('name'); ?> |
 		<?php is_front_page() ? bloginfo('description') : wp_title(''); ?>
